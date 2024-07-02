563e109e7988407fed8c2a5678da2ba25438a08e
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-07-02 16:45:56 -0300

    start project

---
+++ src/main/java/com/rodrigo/springbatch/processadorclassifier/ProcessadorClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.springbatch.processadorclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorClassifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessadorClassifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodrigo/springbatch/processadorclassifier/ProcessadorClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.springbatch.processadorclassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorClassifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
